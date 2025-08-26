73daf669f6b22fe17439b754a6be5aeb9e8af2b5
TangentMochi <69040920+TangentMochi@users.noreply.github.com>
2025-08-26 15:57:41 +0900

    ボタンで現在地を取得

--- lib/views/map.dart
+++ lib/views/map.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:pochi/utils/get_location.dart';
+import 'package:pochi/utils/location_permission_request.dart';
 
 class MyMap extends StatefulWidget {
   @override
@@ -8,13 +10,19 @@ class MyMap extends StatefulWidget {
 }
 
 class _MyMapState extends State<MyMap> {
+  final location = Location();
   late GoogleMapController mapController;
   late LatLng _center = const LatLng(37.7749, -122.4194);
 
-  @override
-  void initState() async {
-    super.initState();
-    setState(() {});
+  void button() {
+    RequestLocationPermission.request(location).then((_) {
+      getPosition(location).then((pos) {
+        print(pos);
+        mapController.moveCamera(
+          CameraUpdate.newLatLng(LatLng(pos.latitude!, pos.longitude!)),
+        );
+      });
+    });
   }
 
   @override
@@ -26,6 +34,10 @@ class _MyMapState extends State<MyMap> {
           mapController = controller;
         },
         initialCameraPosition: CameraPosition(target: _center, zoom: 10),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: button,
+        child: Icon(Icons.my_location),
       ),
     );
   }
